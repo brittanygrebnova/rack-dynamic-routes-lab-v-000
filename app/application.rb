4bd497a7100500c34fc2169567231fc2c81d9f35
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-07-12 18:24:12 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -11,9 +11,7 @@ class Application
       item_name = req.path.split("/items/").last
       #binding.pry
       if item = @@items.find{|i| i.name == item_name}
-          #@@items.each do |i|
-          
-            resp.write "#{item.price}" 
+        resp.write "#{item.price}" 
       else
         resp.write "Item not found"
         resp.status = 400 
