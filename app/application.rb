bf938cf6af44492d35535990507a42d22b677faa
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-07-12 18:02:42 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,11 +9,11 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+                binding.pry
       if @@items.include?(item_name)
           @@items.each do |i|
             resp.write "#{item_name.price}" 
           end
-          binding.pry
       else
         resp.write "Item not found"
         resp.status = 400 
