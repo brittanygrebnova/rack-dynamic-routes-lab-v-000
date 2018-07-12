2856ed1d18237db4d2206d5e6644d0a3d1a4e9e8
Brittany Grebnov <brittanygrebnova@gmail.com>
2018-07-12 18:11:22 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -9,6 +9,7 @@ class Application
     req = Rack::Request.new(env)
     if req.path.match(/items/)
       item_name = req.path.split("/items/").last
+      binding.pry
       if @@items.include?(item_name)
           @@items.each do |i|
             resp.write "#{i.price}" 
